8984760f943158b99fc5a3c6c5368996099097c7
Maksim <103666491+MaksimYakovenko@users.noreply.github.com>
2023-10-08 21:35:01 +0300

    Lab1

--- Lab1/task2.py
+++ Lab1/task2.py
@@ -4,28 +4,45 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 
 data = pd.read_csv("data.csv")
-x = data[data['special_proposition'] > 0]
-average_cost = x["total"].mean()
+prices = data["special_proposition"]
+average_cost = prices.mean()
 print(f"The average cost of the goods from s.p. μ {average_cost}")
 
 
-n = len(x)
+n = len(prices)
+std_error = prices.std() / np.sqrt(n)
 confidence_levels = [0.95, 0.99]
 confidence_intervals = []
-std_error = x["total"].std() / np.sqrt(n)
 for confidence_level in confidence_levels:
-    margin_of_error = stats.norm.pdf((1 + confidence_level) / 2) * std_error
+    alpha = 1 - confidence_level
+    z_critical = stats.norm.ppf(1 - alpha / 2)
+    margin_of_error = z_critical * std_error
     confidence_interval = (average_cost - margin_of_error, average_cost + margin_of_error)
     confidence_intervals.append(confidence_interval)
 
 for i, confidence_level in enumerate(confidence_levels):
-    print(f"Confidence intervals {confidence_level * 100}%:{confidence_intervals[i]}")
+    print(f"Confidence level {confidence_level * 100}% для μ:{confidence_intervals[i]}")
 
 
-plt.figure(figsize=(10, 6))
-plt.hist(x, bins=20, alpha=0.7)
+plt.hist(prices, color="black")
 plt.title("Histogram")
-plt.xlabel("Price of product")
+plt.xlabel("The price of the product according to s.p.")
 plt.ylabel("Frequency")
+plt.title("The histogram of the prices of goods by s.p.")
+plt.grid(True)
+
+
+def edf(data): # Empirical distribution function
+    n = len(data)
+    x = np.sort(data)
+    y = np.arange(1, n + 1) / n
+    return x, y
+
+x, y = edf(prices)
+plt.figure()
+plt.plot(x, y, color="r")
+plt.title("Empirical distribution function")
+plt.xlabel("The price of the product according to s.p.")
+plt.ylabel("Empirical probability")
 plt.grid(True)
 plt.show()
